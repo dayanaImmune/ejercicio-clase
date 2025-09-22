14afa8d8e5a1d8282a1b9943e80a79fcbe5fd582
Dayana <dayana.romero@immune.institute>
2025-09-22 16:08:52 +0200

    ejercicio memoria

--- clase8/memoria/main.js
+++ clase8/memoria/main.js
@@ -2,11 +2,17 @@ const allDiv = document.querySelectorAll(".div")
 
 const handleDiv = (ev)=>{
     const back = ev.target;
-    if(back.classList.contains("back")){
+    ev.currentTarget.classList.toggle("rotate")
+    /*if(back.classList.contains("back")){
         back.classList.add("hidden")
-    }
+    }*/
 }
-
+/*
+const handleMouse = (ev)=>{
+    ev.currentTarget.classList.toggle("rotate")
+}
+*/
 for(const div of allDiv){
     div.addEventListener("click", handleDiv)
+   // div.addEventListener("mouseover", handleMouse)
 }
